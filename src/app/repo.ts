2d6e07b2ef78c3be163620625aac072a492a27e9
Ahonoryoshi <yoshiahona@gmail.com>
2022-04-23 12:06:40 +0300

    Auto-generated commit [ci skip]

--- src/app/repo.ts
+++
@@ -1,7 +0,0 @@
-export class Repo {
-    constructor(
-        public name:string,
-        public bio:string,
-        public created_at:Date,
-        public html_url:string){}
-}
--- src/app/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    constructor(
-        public login:string,
-        public avatar_url:string,
-        public url:string,
-        public id:number,
-        public public_repos:number){}
-}
